365872cc3a10f21a764757e906c87d31030bcfba
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-04 19:04:05 +0000

    Feat: implemented SSG (Static Site Generation) for products home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
-
-import shirt1 from '../assets/shirts/1.png'
-import shirt2 from '../assets/shirts/2.png'
-import shirt3 from '../assets/shirts/3.png'
-import shirt4 from '../assets/shirts/4.png'
 
 import { stripe } from "../lib/stripe";
 import Stripe from "stripe";
@@ -49,7 +44,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -68,6 +63,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
