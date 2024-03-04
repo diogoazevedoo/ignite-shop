62e527ef528a43299486c649ea6e237e271848ed
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-04 15:30:33 +0000

    UI: create initial header component on _app.tsx using next Image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import { AppProps } from "next/app"
+import Image from "next/image"
+
 import { globalStyles } from "../styles/global"
+import { Container, Header } from "../styles/pages/app"
+
+import logo from '../assets/logo.svg'
+
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
