1014c1659b78446f86eea8277cf1f079a71c86f5
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-04 19:39:33 +0000

    UI: create ui for product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from 'next/router';
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Shirt X</h1>
+                <span>27,99$</span>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo esse voluptatibus repellat rerum praesentium molestias dignissimos, deleniti itaque sequi. Exercitationem sunt excepturi quod quidem minus odio minima necessitatibus dignissimos quaerat.</p>
+
+                <button>Buy now</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
