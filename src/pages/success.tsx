bde51070bae8b3a2c9c9c6d8816905f31ef03edb
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-06 18:06:23 +0000

    UI: create ui for success purchase page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
+import { ImageContainer } from "../styles/pages/success";
+import { SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Purchase completed!</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Hello <strong>Diogo Azevedo</strong>, your <strong>Shirt X</strong> is already on his way to your home.
+            </p>
+
+            <Link href='/'>
+                Back to catalog
+            </Link>
+        </SuccessContainer>
     )
 }
